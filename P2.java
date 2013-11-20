7b86700cd3068cd10de0752a53ef6657aabb85dc
amber-h <amber.houle3@gmail.com>
2013-11-20 11:36:51 +0100

    solving problem 2

--- P2.java
+++ P2.java
@@ -1,7 +1,28 @@
 public final class P2{
 
 	public static void main(String[] args){
-		
+		System.out.println(new P2().solve());
 	}
 
+	public int solve(){
+		int n = 0;
+		int sum = 0;
+		int fiboNum = 0;
+		while (fiboNum < 4000000) {
+			fiboNum = fiboFinder(n);
+			if ( fiboNum % 2 == 0 )
+				sum += fiboNum;
+			n++;
+		}
+		return sum;
+	}
+
+	public int fiboFinder(int n){
+		int fiboNum;
+		if ( n == 0 || n == 1 )
+			fiboNum = n;
+		else 
+			fiboNum = fiboFinder(n-1) + fiboFinder(n-2);
+		return fiboNum;
+	}
 }
