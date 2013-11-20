08683b4c5cb87abaa74995f3489d2d43871b1140
amber-h <amber.houle3@gmail.com>
2013-11-20 16:09:40 +0100

    solving problem 3

--- P3.java
+++ P3.java
@@ -1,6 +1,19 @@
+//Prime factor: prime number that divides the integer evenly 
 public final class P3{
 
 	public static void main(String[] args){
-		
+		long number = 600851475143l;
+		System.out.println(new P3().solve(number));
+	}
+
+	public static int solve(long number){
+		int i;
+		for (i = 2; i <= number; i++){
+			if (number % i == 0){
+				number /= i;
+				i--;
+			}
+		}
+		return i;
 	}
 }
